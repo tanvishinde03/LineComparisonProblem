c3dec2865ddc68b803087857aba379a4ad0a4c8e
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-06 00:09:58 +0530

    [Tanvi] Add ; Calculating Line Length

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLength length = new CalculateLength(4, 8, 3, 5);
+            length.Calculate();
+
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+    }
+    }
+
