bbcaed6723d308adcdcccb32c369cfe1280fdd4c
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-06 00:33:19 +0530

    [Tanvi] Add : Checking Line Equality

---
+++ LineComparisonProblem/EqualityBetweenLines.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    public class EqualityBetweenLines
+    {
+        int lengthX, _lengthY, lenGthsX, lengthsY;
+        public EqualityBetweenLines(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine("Lenght of the First line is : " + length);
+            return length;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,16 @@ namespace LineComparisonProblem
         {
             CalculateLength length = new CalculateLength(4, 8, 3, 5);
             length.Calculate();
+
+            // UC2
+
+            EqualityBetweenLines length_Two = new EqualityBetweenLines(5, 6, 7, 8);
+            double length_ = length_Two.Calculate();
+
+            if (length.Equals(length_))
+                Console.WriteLine("Both lines are equal");
+            else
+                Console.WriteLine("Both lines are not equal");
 
             Console.ReadLine();
         }
